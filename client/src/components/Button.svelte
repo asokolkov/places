097fa3ffe81aaec15d740c3ce80cf97381be4924
Aleksey <a.e.sokolkov@gmail.com>
2023-11-08 17:09:55 +0500

    feat: button

---
+++ client/src/components/Button.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+    import type { ComponentType } from 'svelte';
+
+    export let type: 'primary' | 'secondary' | 'tertiary' | 'destructive';
+    export let icon: ComponentType | undefined = undefined;
+    export let text: string;
+    export let onClick: () => void = () => {};
+
+    const bgColors = {
+        primary: 'bg-primary',
+        secondary: 'bg-black',
+        tertiary: 'bg-white',
+        destructive: 'bg-white'
+    };
+    const textColors = {
+        primary: 'text-black',
+        secondary: 'text-white',
+        tertiary: 'text-black',
+        destructive: 'text-error'
+    };
+</script>
+
+<button
+        class={`inline-flex ${bgColors[type]} ${textColors[type]} shadow-element rounded-global h-element shrink-0 items-center justify-center`}
+        on:click={onClick}
+>
+    {#if icon !== undefined}
+        <svelte:component this={icon} />
+    {/if}
+    {text}
+</button>
