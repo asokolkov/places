2b0970661529e2d2cec8bad581b0b32f9f01cf18
Aleksey <a.e.sokolkov@gmail.com>
2023-11-08 15:42:23 +0500

    feat: input

---
+++ client/src/components/Input.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import IconSearch from '$lib/icons/IconSearch.svelte';
+    import IconText from '$lib/icons/IconText.svelte';
+    import IconPassword from '$lib/icons/IconPassword.svelte';
+
+    export let type: 'number' | 'search' | 'text' | 'email' | 'password';
+    export let placeholder: string;
+
+    let inputElement: HTMLInputElement;
+    let inputValue: string = '';
+    let labelColor: string;
+
+    const icons = {
+        number: IconText,
+        search: IconSearch,
+        text: IconText,
+        email: IconText,
+        password: IconPassword,
+    };
+
+    $: labelColor = inputValue ? 'text-black' : 'text-inactive';
+
+    onMount(() => {
+        inputElement.type = type;
+    });
+</script>
+
+<label on:focus={() => inputElement.focus()} class={`flex px-m items-center gap-s self-stretch rounded-global bg-white ${labelColor}`}>
+    <svelte:component this={icons[type]} />
+    <input bind:this={inputElement} bind:value={inputValue} {placeholder} class="w-full h-element appearance-none caret-black bg-search outline-0" />
+</label>
