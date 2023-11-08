6f90b33ae2ac069af8432cde0f93e1ef669f5768
Aleksey <a.e.sokolkov@gmail.com>
2023-11-08 18:31:29 +0500

    fix: removed unused components

--- client/src/components/BigHeader.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    export let text: string;
-    export let hint: string;
-</script>
-
-<section class="flex flex-col gap-s px-s">
-    <h1 class="text-h1">{text}</h1>
-    <h1 class="text-h2 text-inactive">{hint}</h1>
-</section>
--- client/src/components/IconButton.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-    import IconSearch from '$lib/icons/IconSearch.svelte';
-
-    export let type: 'button' | 'submit' | 'reset' | null | undefined;
-</script>
-
-<button {type} class="flex h-element w-element rounded-global justify-center items-center text-black bg-white">
-    <IconSearch />
-</button>
