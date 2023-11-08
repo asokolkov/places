4d87dd0de9978d31b61379a11066916a82fb38ea
Aleksey <a.e.sokolkov@gmail.com>
2023-11-08 17:32:25 +0500

    feat: signin and signup pages

---
+++ client/src/routes/signin/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import Input from 'components/Input.svelte';
+    import Button from 'components/Button.svelte';
+    import IconSignIn from '$lib/icons/IconSignIn.svelte';
+    import { goto } from '$app/navigation';
+</script>
+
+<h1>Войти в аккаунт</h1>
+<Input type="email" placeholder="Почта" />
+<Input type="password" placeholder="Пароль" />
+<Button type="primary" icon={IconSignIn} text="Войти" />
+<Button type="tertiary" text="Зарегистрироваться" onClick={() => goto('/signup')} />
---
+++ client/src/routes/signup/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import Input from 'components/Input.svelte';
+    import Button from 'components/Button.svelte';
+    import IconSignIn from '$lib/icons/IconSignIn.svelte';
+    import { goto } from '$app/navigation';
+</script>
+
+<h1>Зарегистрироваться</h1>
+<Input type="text" placeholder="Имя пользователя" />
+<Input type="text" placeholder="Логин" />
+<Input type="email" placeholder="Почта" />
+<Input type="password" placeholder="Пароль" />
+<Input type="password" placeholder="Повторите пароль" />
+<Button type="primary" icon={IconSignIn} text="Зарегистрироваться" />
+<Button type="tertiary" text="Войти" onClick={() => goto('/signin')} />
