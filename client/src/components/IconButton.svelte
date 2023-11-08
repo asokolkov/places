7cfaf3eb7c40d56ace120db97981f55f35df8d93
Aleksey <a.e.sokolkov@gmail.com>
2023-11-08 14:37:04 +0500

    fix: more icons

---
+++ client/src/components/IconButton.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import IconSearch from '$lib/icons/IconSearch.svelte';
+
+    export let type: 'button' | 'submit' | 'reset' | null | undefined;
+</script>
+
+<button {type} class="flex h-element w-element rounded-global justify-center items-center text-black bg-white">
+    <IconSearch />
+</button>
