28cd99554b513eed82f355bbab116c0b4e968ad0
Aleksey <a.e.sokolkov@gmail.com>
2023-11-08 16:48:41 +0500

    feat: card

---
+++ client/src/components/Card.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    export let text: string;
+    export let hint: string;
+    export let active: boolean = false;
+    export let extended: boolean;
+
+    const sizes = extended ? 'w-[256px] h-[256px]' : '';
+    const bgColor = active ? 'bg-black' : 'bg-white';
+    const textColor = active ? 'text-white' : 'text-black';
+</script>
+
+<section class={`element shrink-0 ${sizes} flex-col justify-end items-center gap-s ${bgColor}`}>
+    <p class={`flex self-stretch ${textColor}`}>{text}</p>
+    <p class="flex self-stretch text-inactive">{hint}</p>
+</section>
